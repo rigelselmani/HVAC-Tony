4cae8ca1c87f99f35d9460042d4bc6eccac58d4f
Rigel <rigel.selmani@gmail.com>
2023-06-05 18:19:04 -0400

    footer change h1

--- tonyhvac/src/components/Footer/Footer.js
+++ tonyhvac/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import "./Footer.css"
 export default function Nav(){
     return (
         <footer>
-            <a className="navbar-brand text-primary fs-2 fst-italic " style={{fontWeight:"bold", fontSize:"40px"}} href="/"><span className="text-bg-dark px-4 m-0">HVAC</span> Tony</a>
+            <a className="navbar-brand text-primary fs-2 fst-italic " style={{fontWeight:"bold", fontSize:"40px"}} href="/"><span className="text-bg-dark px-4 m-0">GR</span> Comfort</a>
             <p className="text-light mt-3">HOURS
               Monday - Friday
               8:00 AM - 5:00 PM</p>
